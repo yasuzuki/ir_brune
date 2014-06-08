da9c64a3a9bceaadb6698cae785b715f3154e913
Yasunori Suzuki <railsyas@gmail.com>
2014-06-08 11:32:34 +0900

    convert from .erb to .haml.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140608013648) do
+ActiveRecord::Schema.define(version: 20140608021908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "devices", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "messages", force: true do |t|
     t.integer  "device_id"
