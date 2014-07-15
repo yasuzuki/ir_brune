80d363305dd5e5e619e58272537a2cacbcd95936
Yasunori Suzuki <railsyas@gmail.com>
2014-07-15 22:59:57 +0900

    add a method formatting for api data.

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,14 +4,20 @@ class Message < ActiveRecord::Base
   base_uri 'https://api.getirkit.com'
 
   def send_data
-    Rails.logger.info "#{self.to_json}"
     data = { body:
       {
         clientkey: Station.client_key,
         deviceid: Station.device_id,
-        message: self.to_json(only: [:format, :freq, :data])
+        message: self.to_api
       }
     }
     self.class.post('/1/messages', data)
+  end
+
+  def to_api
+    Rails.logger.info "#{self.as_json}"
+    message = self.as_json(only: [:format, :freq, :data])
+    message['data'] = message['data'].split(',').map(&:to_i)
+    message.to_json
   end
 end
