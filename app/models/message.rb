c50bf9f263d778fb32c34c1dbf4e22a94e181387
Yasunori Suzuki <railsyas@gmail.com>
2014-07-15 22:33:59 +0900

    add send_data method.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,17 @@
 class Message < ActiveRecord::Base
+  include HTTParty
   belongs_to :device
+  base_uri 'https://api.getirkit.com'
+
+  def send_data
+    Rails.logger.info "#{self.to_json}"
+    data = { body:
+      {
+        clientkey: Station.client_key,
+        deviceid: Station.device_id,
+        message: self.to_json(only: [:format, :freq, :data])
+      }
+    }
+    self.class.post('/1/messages', data)
+  end
 end
